550911b4ee77d27b3193090f976dcd477e480f11
m.ahmed <m.ahmed@easternenterprise.com>
2022-10-01 01:54:45 +0200

    (Added) Todo list is added with todo items.

--- src/UI/buttons/Buttons.component.jsx
+++ src/UI/buttons/Buttons.component.jsx
@@ -1,6 +1,10 @@
 export const BUTTON_TYPES_CLASSES = {
-  "primary": "bg-primary text-white hover:bg-white hover:text-primary",
-  "white": "bg-white text-primary hover:bg-primary hover:text-white",
+  primary:
+    "bg-primary text-white border-primary hover:bg-white hover:text-primary",
+  white:
+    "bg-white text-primary border-primary hover:bg-primary hover:text-white",
+  green:
+    "bg-green-500 text-white border-green-500 hover:bg-white hover:text-green-500",
 };
 
 const Button = (props) => {
@@ -8,7 +12,7 @@ const Button = (props) => {
     <button
       className={`${BUTTON_TYPES_CLASSES[props.type]} ${
         props.classnames ? props.classnames : ""
-      } px-4 py-2 rounded transition duration-300 border border-primary`}
+      } px-4 py-2 rounded transition duration-300 border`}
       {...props}
     >
       {props.children}
--- src/components/todo-list/TodoList.component.jsx
+++ src/components/todo-list/TodoList.component.jsx
@@ -1,10 +1,41 @@
+import { useState } from "react";
+import { ReactSortable } from "react-sortablejs";
+import Button from "../../UI/buttons/Buttons.component";
+import TodoItem from "./todo-item/TodoItem.component";
 
 const TodoList = () => {
-  return (
-    <div>
-      Todo List
-    </div>
-  )
-}
+  const [todos, setTodos] = useState([
+    { mission: "Todo 1", isDone: false, id: 1 },
+    { mission: "Todo 2", isDone: true, id: 2 },
+    { mission: "Todo 3", isDone: true, id: 3 },
+  ]);
 
-export default TodoList
+  let todosContent = <p className="text-lg">Create a new todo!</p>;
+
+  if (todos) {
+    todosContent = (
+      <ReactSortable list={todos} setList={setTodos} animation={200} swap={true}>
+        {todos.map((todo) => (
+          <TodoItem key={todo.id} todo={todo} />
+        ))}
+      </ReactSortable>
+    );
+  }
+  return (
+    <div className="w-1/2">
+      <div className="p-4 w-full bg-white rounded-lg border shadow-md sm:p-8">
+        <div className="flex justify-between items-center mb-4">
+          <h5 className="text-xl font-bold text-gray-900">Todo list</h5>
+          <Button type="green">Add Todo</Button>
+        </div>
+        <div className="flow-root">
+          <ul role="list" className="divide-y divide-gray-200 ">
+            {todosContent}
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default TodoList;
